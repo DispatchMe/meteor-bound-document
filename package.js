0a2285d09967c210940cf94e33400c475bce7c34
Jason Raede <jason.raede@gmail.com>
2015-09-01 17:30:06 -0400

    Set version constraints

--- package.js
+++ package.js
@@ -4,12 +4,12 @@ Package.describe({
   version: '0.0.1'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
   api.use([
-    'underscore',
-    'check',
-    'minimongo',
-    'aldeed:simple-schema',
+    'underscore@1.0.3',
+    'check@1.0.5',
+    'minimongo@1.0.8',
+    'aldeed:simple-schema@1.3.3',
     'aldeed:collection2@2.4.0'
   ]);
 
@@ -21,7 +21,7 @@ Package.onUse(function (api) {
   api.export('BoundDocument');
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
   api.use('sanjo:jasmine@0.16.4');
 
   api.use([
